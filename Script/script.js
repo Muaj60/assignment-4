a6d93da80403624445329d8a79ced629b0226e76
Muaj60 <m.studyboy49@gmail.com>
2026-02-23 14:29:14 +0600

    Calculation Part

--- Script/script.js
+++ Script/script.js
@@ -19,3 +19,30 @@ for (let card of allCard) {
   let status = card.querySelector(".status");
   status.innerText = "Not Applied";
 }
+
+function removeArray(arr, card) {
+  let newArr = [];
+  for (let item of arr) {
+    if (item !== card) {
+      newArr.push(item);
+    }
+  }
+  return newArr;
+}
+
+function CalculateCount() {
+  allCard = document.querySelectorAll(".job-card");
+  total.innerText = allCard.length;
+
+  interviewCount.innerText = interviewList.length;
+  rejectedCount.innerText = rejectedList.length;
+
+  if (currentFilter == "Interview") {
+    allJob.innerText = interviewList.length + " of " + allCard.length + " jobs";
+  } else if (currentFilter == "Rejected") {
+    allJob.innerText = rejectedList.length + " of " + allCard.length + " jobs";
+  } else {
+    allJob.innerText = allCard.length + " jobs";
+  }
+}
+CalculateCount();
